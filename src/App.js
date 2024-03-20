d95da4a535ca12b29e37e03a8d91a4e4ca01c851
kangsuyeong <ekdh16@naver.com>
2024-03-20 21:59:15 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import {useState} from "react";
-import './App.css';
-import Box from "./component/Box"
+import { useState } from "react";
+import "./App.css";
+import Box from "./component/Box";
 
 // 1. 박스 2개 (타이틀,사진,결과)
 // 2. 가위 바위 보 버튼이 있다.
@@ -10,37 +10,75 @@ import Box from "./component/Box"
 // 6. 승패결과에 따라 테두리 색이 바뀐다 (이기면-초록, 지면-빨강, 비기면 -검은색)
 
 const choice = {
-  rock:{
-    name:"Rock",
-    img : "https://img.freepik.com/free-vector/isolated-rock-in-nature-cartoon_1308-135422.jpg"
+  rock: {
+    name: "Rock",
+    img: "images/rock2.jpg",
   },
-  scissors:{
-    name:"Scissors",
-    img : "https://png.pngtree.com/png-vector/20190227/ourmid/pngtree-vector-scissor-icon-png-image_707288.jpg"
+  scissors: {
+    name: "Scissors",
+    img: "images/scissors2.jpg",
   },
-  paper:{
-    name:"Paper",
-    img : "https://png.pngtree.com/element_our/20190603/ourmid/pngtree-colored-writing-paper-illustration-image_1432850.jpg"
+  paper: {
+    name: "Paper",
+    img: "images/paper2.jpg",
+  },
+  default: {
+    name: "default",
+    img: "images/game.jpg",
   }
-}
+};
 function App() {
+  //state
+  const [userSelect, setUserSelect] = useState(choice.default);
+  const [computerSelect, setComputerSelect] = useState(choice.default);
+  const [result, setResult] = useState("");
 
-  const [userSelect,setUserSelect] = useState(null)
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice(); //컴퓨터가 선택한 값
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice))
+  };
 
-  const play = (userChoice)=>{
-    setUserSelect(choice[userChoice])
-  }
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    // user == computer tie
+    // user == rock, computer ==scissors user 이긴거지
+    // user == "rock" computer ==paper  user 패
+    // user == scissors computer == paper user 승
+    // user == scissors computer == rack user 패
+    // user == paper computer == rock user승
+    // user == paper computer == scissor user패
+
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+      return computer.name === "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어주는 함수
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
 
   return (
     <div>
-      <div className='main'>
-        <Box title='You' item={userSelect}/>
-        {/* <Box title='Computer'/> */}
+      <div className="main">
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
-      <div className='main'>
-        <button onClick={()=>play("scissors")}>가위</button>
-        <button onClick={()=>play("rock")}>바위</button>
-        <button onClick={()=>play("paper")}>보</button>
+      <div className="main">
+        <button className="button-size" onClick={() => play("scissors")}><img className="button-img" src="images/scissors2.jpg"></img></button>
+        <button className="button-size" onClick={() => play("rock")}><img className="button-img" src="images/rock2.jpg"></img></button>
+        <button className="button-size" onClick={() => play("paper")}><img className="button-img" src="images/paper2.jpg"></img></button>
       </div>
     </div>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Box = (props) => {
-    console.log("props",props)
+  console.log("props",props)
+  let result
+  if(props.title==="Computer" && props.result!=="tie" && props.result!==""){
+    result = props.result==="win" ? "lose" : "win"
+  }
+  else{
+    result=props.result
+  }
   return (
-    <div className="box">
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       <img className="item-img" src={props.item && props.item.img}></img>
-      <h2>Win!</h2>
+      <h2 className={`${result}Color`}>{result}</h2>
     </div>
   )
 }
